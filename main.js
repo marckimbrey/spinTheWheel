03a1e698ad32064fde622cfbf22a15b13de49367
marckimbrey <marckimbrey@gmail.com>
2018-06-07 07:35:47 +0100

    Add addRow and deleteRow eventhandlers

--- main.js
+++ main.js
@@ -43,14 +43,82 @@ function spinWheel(spinLength) {
 // handle slice input data
 function loadSliceInputs(data) {
 
-  data.forEach((slice) => {
+  data.forEach((slice, i) => {
     let newSlice = document.createElement("div");
     newSlice.innerHTML = `
                 <label>Option</label>
                 <input type="text" name="name" value="${slice.name}">
                 <label>color</label>
                 <input type="color" name="color" value="${slice.color}">
+                <button
+                  class="delete-slice-btn btn"
+                  type="button"
+                  name="deleteSlice"
+                  >X</button>
               `;
-      document.getElementsByClassName('wheel-data')[0].appendChild(newSlice)
+      if (i >= data.length -1) {
+        newSlice.innerHTML += `
+          <button class="add-slice-btn btn" type="button" name="addSlice">
+          +</button>`;
+      };
+      document.getElementsByClassName('wheel-data-inputs')[0].appendChild(newSlice)
   })
 }
+
+/***
+ FORM EVENT HANDLERS
+ ***/
+
+ const deleteBtns = document.getElementsByClassName("delete-slice-btn");
+ const addSlice = document.getElementsByClassName("add-slice-btn")[0];
+
+// delete input row
+function deleteSlice() {
+  // if last row
+  if (!this.parentNode.nextSibling) {
+    this.parentNode.previousSibling.innerHTML += `
+      <button
+        class="add-slice-btn btn"
+        type="button"
+        name="addSlice">
+      +</button>`;
+    this.parentNode.previousSibling.lastChild.addEventListener('click', addRow);
+  }
+  this.parentNode.parentNode.removeChild(this.parentNode)
+}
+
+function addRow() {
+  console.log(this)
+  this.parentNode.removeChild(this);
+  let newSlice = document.createElement("div");
+  newSlice.innerHTML = `
+              <label>Option</label>
+              <input type="text" name="name" value="">
+              <label>color</label>
+              <input type="color" name="color" value="">
+              <button
+                class="delete-slice-btn btn"
+                type="button"
+                name="deleteSlice"
+                >X</button>
+              <button class="add-slice-btn btn" type="button" name="addSlice">
+              +</button>
+            `;
+  // add onclick eventlisteners
+  Array.prototype.forEach.call(newSlice.childNodes, function(el, i) {
+      if (el.className == "delete-slice-btn btn") {
+        newSlice.childNodes[i].addEventListener('click', deleteSlice)
+      } else if (el.className == "add-slice-btn btn") {
+        newSlice.childNodes[i].addEventListener('click', addRow)
+      }
+  })
+
+  document.getElementsByClassName('wheel-data-inputs')[0].appendChild(newSlice)
+}
+
+
+Array.prototype.forEach.call(deleteBtns, function(btn) {
+  btn.addEventListener("click", deleteSlice)
+});
+
+addSlice.addEventListener("click", addRow)
